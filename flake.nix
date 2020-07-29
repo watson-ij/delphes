9d88ba21b03f068c71c8d981329914b92676adbc
Ian J. Watson <ian.james.watson@cern.ch>
2020-07-29 17:43:32 +0900

    Add nix flake for easy building

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nix flake for Delphes";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs } : let
+    system = "x86_64-linux";
+  in {
+    overlay = final: prev: {
+      delphes = with final; stdenv.mkDerivation {
+        name = "delphes";
+        version = "3.4.x";
+        nativeBuildInputs = [ cmake tcl ];
+        buildInputs = [ root lhapdf pythia ];
+        src = self;
+        PYTHIA8=pythia;
+      };
+    };
+
+    defaultPackage.${system} = (import nixpkgs { inherit system; overlays = [self.overlay]; }).delphes;
+  };
+}
