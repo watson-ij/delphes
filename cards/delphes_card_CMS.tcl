d0218df9c1bee759d61ba879fbc308909ef7b9aa
Ian J. Watson <ian.james.watson@cern.ch>
2020-07-29 17:51:51 +0900

    CMS card for use with Woojin's TrackSmearing

--- cards/delphes_card_CMS.tcl
+++ cards/delphes_card_CMS.tcl
@@ -14,7 +14,8 @@ set ExecutionPath {
   MuonMomentumSmearing
 
   TrackMerger
- 
+  TrackSmearing 
+
   ECal
   HCal
  
@@ -46,8 +47,10 @@ set ExecutionPath {
   JetEnergyScale
 
   JetFlavorAssociation
+  GenJetFlavorAssociation
 
   BTagging
+  GenBTagging
   TauTagging
 
   UniqueObjectFinder
@@ -68,6 +71,7 @@ module ParticlePropagator ParticlePropagator {
   set ChargedHadronOutputArray chargedHadrons
   set ElectronOutputArray electrons
   set MuonOutputArray muons
+  #set V0OutputArray v0
 
   # radius of the magnetic field coverage, in m
   set Radius 1.29
@@ -77,6 +81,20 @@ module ParticlePropagator ParticlePropagator {
   # magnetic field
   set Bz 3.8
 }
+
+#################################
+# Decay Filter
+#################################
+
+#module DecayFilter DecayFilter {
+#  set InputArray FastJetFinder/jets
+#  #set InputArray TrackSmearing/tracks
+#  #add InputArray ParticlePropagtor/stableParticles
+#  #add InputArray ParticlePropagator/chargedHadrons
+#
+#  set OutputArray tracks
+#
+#}
 
 ####################################
 # Charged hadron tracking efficiency
@@ -152,9 +170,15 @@ module MomentumSmearing ChargedHadronMomentumSmearing {
 
   # resolution formula for charged hadrons
   # based on arXiv:1405.6569
-  set ResolutionFormula {                  (abs(eta) <= 0.5) * (pt > 0.1) * sqrt(0.06^2 + pt^2*1.3e-3^2) +
-                         (abs(eta) > 0.5 && abs(eta) <= 1.5) * (pt > 0.1) * sqrt(0.10^2 + pt^2*1.7e-3^2) +
-                         (abs(eta) > 1.5 && abs(eta) <= 2.5) * (pt > 0.1) * sqrt(0.25^2 + pt^2*3.1e-3^2)}
+  #set ResolutionFormula {                  (abs(eta) <= 0.5) * (pt > 0.1) * sqrt(0.06^2 + pt^2*1.3e-3^2) +
+  #                       (abs(eta) > 0.5 && abs(eta) <= 1.5) * (pt > 0.1) * sqrt(0.10^2 + pt^2*1.7e-3^2) +
+  #                       (abs(eta) > 1.5 && abs(eta) <= 2.5) * (pt > 0.1) * sqrt(0.25^2 + pt^2*3.1e-3^2)}
+ 
+  # Rough matching to 1405.6569 fig. 15
+  set ResolutionFormula {                  (abs(eta) <= 0.9) * (pt > 0.1) * (0.1606)*sqrt(0.06^2 + pt^2*1.3e-3^2) +
+                         (abs(eta) > 0.9 && abs(eta) <= 1.4) * (pt > 0.1) * (0.1622)*sqrt(0.10^2 + pt^2*1.7e-3^2) +
+                         (abs(eta) > 1.4 && abs(eta) <= 2.5) * (pt > 0.1) * (0.1758)*sqrt(0.25^2 + pt^2*3.1e-3^2)}
+
 }
 
 ###################################
@@ -202,6 +226,22 @@ module Merger TrackMerger {
   set OutputArray tracks
 }
 
+################################                                                                    
+## Track impact parameter smearing                                                                   
+#################################                                                                    
+
+module TrackSmearing TrackSmearing {
+set InputArray TrackMerger/tracks
+#  set BeamSpotInputArray BeamSpotFilter/beamSpotParticle
+set OutputArray tracks
+#  set ApplyToPileUp true
+
+# magnetic field
+set Bz 3.8
+
+source trackResolutionCMS.tcl
+}
+
 
 
 #############
@@ -210,7 +250,7 @@ module Merger TrackMerger {
 
 module SimpleCalorimeter ECal {
   set ParticleInputArray ParticlePropagator/stableParticles
-  set TrackInputArray TrackMerger/tracks
+  set TrackInputArray TrackSmearing/tracks
 
   set TowerOutputArray ecalTowers
   set EFlowTrackOutputArray eflowTracks
@@ -513,7 +553,7 @@ module Isolation ElectronIsolation {
 
   set OutputArray electrons
 
-  set DeltaRMax 0.5
+  set DeltaRMax 0.3
 
   set PTMin 0.5
 
@@ -547,7 +587,7 @@ module Isolation MuonIsolation {
 
   set OutputArray muons
 
-  set DeltaRMax 0.5
+  set DeltaRMax 0.4
 
   set PTMin 0.5
 
@@ -610,7 +650,7 @@ module FastJetFinder GenJetFinder {
 
   # algorithm: 1 CDFJetClu, 2 MidPoint, 3 SIScone, 4 kt, 5 Cambridge/Aachen, 6 antikt
   set JetAlgorithm 6
-  set ParameterR 0.5
+  set ParameterR 0.4
 
   set JetPTMin 20.0
 }
@@ -639,7 +679,7 @@ module FastJetFinder FastJetFinder {
 
   # algorithm: 1 CDFJetClu, 2 MidPoint, 3 SIScone, 4 kt, 5 Cambridge/Aachen, 6 antikt
   set JetAlgorithm 6
-  set ParameterR 0.5
+  set ParameterR 0.4
 
   set JetPTMin 20.0
 }
@@ -704,11 +744,29 @@ module JetFlavorAssociation JetFlavorAssociation {
   set ParticleLHEFInputArray Delphes/allParticlesLHEF
   set JetInputArray JetEnergyScale/jets
 
-  set DeltaR 0.5
+  set DeltaR 0.4
   set PartonPTMin 1.0
   set PartonEtaMax 2.5
 
 }
+
+########################
+# GenJet Flavor Association
+########################
+
+module JetFlavorAssociation GenJetFlavorAssociation {
+
+  set PartonInputArray Delphes/partons
+  set ParticleInputArray Delphes/allParticles
+  set ParticleLHEFInputArray Delphes/allParticlesLHEF
+  set JetInputArray GenJetFinder/jets
+
+  set DeltaR 0.4
+  set PartonPTMin 1.0
+  set PartonEtaMax 2.5
+
+}
+
 
 ###########
 # b-tagging
@@ -724,15 +782,43 @@ module BTagging BTagging {
   # gluon's PDG code has the lowest priority
 
   # based on arXiv:1211.4462
-  
+
+  # From seulgi's card setting
+
   # default efficiency formula (misidentification rate)
-  add EfficiencyFormula {0} {0.01+0.000038*pt}
+  add EfficiencyFormula {0} {0.007+0.000029*pt}
 
   # efficiency formula for c-jets (misidentification rate)
-  add EfficiencyFormula {4} {0.25*tanh(0.018*pt)*(1/(1+ 0.0013*pt))}
+  add EfficiencyFormula {4} {0.13*tanh(0.045*pt)*(1/(1+ 0.00008*pt))}
 
   # efficiency formula for b-jets
-  add EfficiencyFormula {5} {0.85*tanh(0.0025*pt)*(25.0/(1+0.063*pt))}
+  add EfficiencyFormula {5} {0.72*tanh(0.0345*pt)*(1/(1+0.00077*pt))}
+  
+}
+
+###########
+# Gen jet b-tagging
+###########
+
+module BTagging GenBTagging {
+  set JetInputArray GenJetFinder/jets
+
+  set BitNumber 0
+
+  # add EfficiencyFormula {abs(PDG code)} {efficiency formula as a function of eta and pt}
+  # PDG code = the highest PDG code of a quark or gluon inside DeltaR cone around jet axis
+  # gluon's PDG code has the lowest priority
+
+  # based on arXiv:1211.4462
+
+  # default efficiency formula (misidentification rate)
+  add EfficiencyFormula {0} {0.0}
+
+  # efficiency formula for c-jets (misidentification rate)
+  add EfficiencyFormula {4} {0.0}
+
+  # efficiency formula for b-jets
+  add EfficiencyFormula {5} {1.0}
 }
 
 #############
@@ -783,7 +869,7 @@ module TreeWriter TreeWriter {
 # add Branch InputArray BranchName BranchClass
   add Branch Delphes/allParticles Particle GenParticle
 
-  add Branch TrackMerger/tracks Track Track
+  add Branch TrackSmearing/tracks Track Track
   add Branch Calorimeter/towers Tower Tower
 
   add Branch HCal/eflowTracks EFlowTrack Track
